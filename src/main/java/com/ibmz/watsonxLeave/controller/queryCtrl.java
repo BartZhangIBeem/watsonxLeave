7b28bb4ef4764d751a4602065d2c87ce6da9440e
BartZhang <zhenyuz@cn>
2024-01-19 10:26:09 +0800

    watsonxConn

--- src/main/java/com/ibmz/watsonxLeave/controller/queryCtrl.java
+++ src/main/java/com/ibmz/watsonxLeave/controller/queryCtrl.java
@@ -1,14 +1,14 @@
 package com.ibmz.watsonxLeave.controller;
 
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
 import com.ibm.db2.jcc.DB2ConnectionPoolDataSource;
 import com.ibmz.watsonxLeave.dto.Employee;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.sql.PooledConnection;
 import java.sql.*;
+import java.util.Map;
 
 @RestController
 
@@ -31,6 +31,30 @@ public class queryCtrl {
     public Employee queryUser(@PathVariable("id") Integer id) throws SQLException, ClassNotFoundException {
         Connection conn = DriverManager.getConnection("jdbc:db2://19af6446-6171-4641-8aba-9dcff8e1b6ff.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud:30699/bludb:user=vrn32906;password=o5Nwlh3rbavWaXVs;sslConnection=true;" );
         Statement stmt = conn.createStatement();
+        String sql = "select * from EMPLOYEE where EMPLOYEEID= "+id;
+        ResultSet resultSet = stmt.executeQuery(sql);
+        Employee employee = new Employee();
+        if (resultSet.next()) {
+            employee.setFirstname(resultSet.getString("firstname"));
+            employee.setDepartment(resultSet.getString("department"));
+            employee.setPosition(resultSet.getString("position"));
+            employee.setLastname(resultSet.getString("lastname"));
+            employee.setHiredate(resultSet.getDate("hiredate"));
+        }
+        employee.setEmpId(id);
+        return employee;
+    }
+
+    @RequestMapping(value = "/user", method = RequestMethod.POST)
+    public Employee queryUser(@RequestBody String param) throws SQLException, ClassNotFoundException {
+        Connection conn = DriverManager.getConnection("jdbc:db2://19af6446-6171-4641-8aba-9dcff8e1b6ff.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud:30699/bludb:user=vrn32906;password=o5Nwlh3rbavWaXVs;sslConnection=true;" );
+        Statement stmt = conn.createStatement();
+
+        JSONObject jsonObject = JSON.parseObject(param);
+
+        int id = jsonObject.getIntValue("userid");
+
+
         String sql = "select * from EMPLOYEE where EMPLOYEEID= "+id;
         ResultSet resultSet = stmt.executeQuery(sql);
         Employee employee = new Employee();
